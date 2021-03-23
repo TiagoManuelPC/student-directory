9e7156896ee784d6862ccf9bcb45907ca77c5bae
TiagoManuelPC <tiagomanuelpereiracampos@hotmail.com>
2021-03-23 22:39:03 +0000

    use from CL

--- directory.rb
+++ directory.rb
@@ -1,27 +1,53 @@
 @students = []
 
+def print_menu
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save the list to students.csv"
+    puts "4. Load the list from students.csv"
+    puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def interactive_menu
+  loop do
+    # 1. print the menu and ask the user what to do
+    print_menu
+    # 2. read the input and save it into a variable
+    process(STDIN.gets.chomp)
+    # 3. do what the user has asked
+ end
+end
+
+def process(selection)
+  case selection
+    when "1"
+    input_students
+    when "2"
+    show_students
+    when "3"
+    save_students
+    when "4"
+    load_students
+    when "9"
+    exit # this will cause the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+  end
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   # get the first name
-  name = gets.chomp
-# while the name is not empty, repeat this code
+  name = STDIN.gets.chomp
+   # while the name is not empty, repeat this code
   while !name.empty? do
-# add the student and cohort hash to the array
+  # add the student and cohort hash to the array
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
     # get another name from the user
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
-end
-
-def load_students
-  file = File.open("students.csv", "r")
-  file.readlines.each do |line|
-  name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_s}
-  end
-  file.close
 end
 
 def show_students
@@ -31,8 +57,8 @@ def show_students
 end
 
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+   puts "The students of Villains Academy"
+   puts "-------------"
 end
 
 def print_students_list
@@ -42,42 +68,11 @@ def print_students_list
 end
 
 def print_footer
+  if @students.count == 1
+  puts "Overall, we have #{@students.count} great student"
+  else
   puts "Overall, we have #{@students.count} great students"
-end
-
-def print_menu
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "3. Save the list to students.csv"
-    puts "4. Load the list from students.csv"
-    puts "9. Exit" # 9 because we'll be adding more items
-end
-
-def process(selection)
-  case selection
-    when "1"
-      input_students
-    when "2"
-      show_students
-    when "3"
-      save_students
-      when "4"
-      load_students
-    when "9"
-      exit # this will cause the program to terminate
-    else
-      puts "I don't know what you meant, try again"
   end
-end
-
-def interactive_menu
-  loop do
-    # 1. print the menu and ask the user what to do
-    print_menu
-    # 2. read the input and save it into a variable
-    process(gets.chomp)
-    # 3. do what the user has asked
- end
 end
 
 def save_students
@@ -88,8 +83,29 @@ def save_students
   student_data = [student[:name], student[:cohort]]
   csv_line = student_data.join(",")
   file.puts csv_line
-end
+  end
 file.close
+end
+
+def load_students(filename ="students.csv")
+  file = File.open(filename, "r")
+  file.readlines.each do |line|
+  name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_s}
+  end
+  file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+     puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
 end
 
 interactive_menu
